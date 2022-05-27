096ec1f5d509af45245c6ff2ef9564cadf269fa8
David Akwuru <daakwuru@gmail.com>
2022-05-27 21:40:08 +0100

    Update main.py

--- Finding-Anagrams/main.py
+++ Finding-Anagrams/main.py
@@ -6,9 +6,9 @@
 
 def find_anagram(word, anagram):
     # assumption ->  we assume the word might also be a compound word seperated by spaces
-    # replace all spaces contained in the word with empty space
-    word = word.replace(" ", "")
-    anagram =  anagram.replace(" ", "")
+    # replace all spaces contained in the word with empty space and also convert to lower case
+    word = word.replace(" ", "").lower()
+    anagram =  anagram.replace(" ", "").lower()
     
     # sort both words in ascending order and check if they are same
     # This return either True for anagram or False if they are not anagrams
